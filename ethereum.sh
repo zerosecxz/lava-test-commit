b31edba5c2bad4338f8e3102fb71c94e8c25383d
zerosecxz <155688584+zerosecxz@users.noreply.github.com>
2024-03-29 09:54:16 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-8247b742-4670-47af-9954-a3d32d28343a/"
 
 OUTPUT_DIR="logs"
 
